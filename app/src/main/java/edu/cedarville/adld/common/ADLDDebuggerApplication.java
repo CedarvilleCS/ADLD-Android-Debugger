f972adbeb22ea4635a8709a11236a64b42e7279d
Daniel Rees <drees@developertown.com>
2016-01-15 17:37:27 -0500

    Initial commit

---
+++ app/src/main/java/edu/cedarville/adld/common/ADLDDebuggerApplication.java
@@ -0,0 +1,6 @@
+package edu.cedarville.adld.common;
+
+import android.app.Application;
+
+public class ADLDDebuggerApplication extends Application {
+}
---
+++ app/src/main/java/edu/cedarville/adld/common/dagger/Components.java
@@ -0,0 +1,4 @@
+package edu.cedarville.adld.common.dagger;
+
+public interface Components {
+}
