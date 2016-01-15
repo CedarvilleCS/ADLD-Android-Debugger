bf902a55af380a6dd2e051b506d61135f415d17e
Daniel Rees <drees@developertown.com>
2016-01-15 18:15:06 -0500

    Added DataPoint and translating CSV list into DataPoint models. Added Unit tests as well

---
+++ app/src/main/java/edu/cedarville/adld/common/model/DataPoint.java
@@ -0,0 +1,94 @@
+package edu.cedarville.adld.common.model;
+
+/**
+ * DataPoint models the data that is received by the device from the robot's bluetooth transmitter.
+ * Each set of data that is transmitted is in the form
+ *
+ * A,B,C,D
+ *
+ * Where A is the left sensor, B is the front sensor, C is the right sensor, and D is the sonar
+ * sensor. Each value, A-D, can be a pair of any two Hex values 00-FF. A valid input would be
+ *
+ * A0,15,82,EF
+ *
+ * This class uses the converted values of the input and makes them available as integers.
+ */
+public class DataPoint {
+
+    /** Integer value of the left sensor for this data point */
+    public final int leftSensor;
+
+    /** Integer value of the front sensor for this data point */
+    public final int frontSensor;
+
+    /** Integer value of the right sensor for this data point */
+    public final int rightSensor;
+
+    /** Integer value of the sonar sensor for this data point */
+    public final int sonarSensor;
+
+    private DataPoint(Builder builder) {
+        leftSensor = builder.leftSensor;
+        frontSensor = builder.frontSensor;
+        rightSensor = builder.rightSensor;
+        sonarSensor = builder.sonarSensor;
+    }
+
+
+    ////
+    ////// Class Methods
+    ////
+    public String getLeftSesnorHexValue() {
+        return Integer.toHexString(leftSensor);
+    }
+
+    public String getFrontSesnorHexValue() {
+        return Integer.toHexString(frontSensor);
+    }
+
+    public String getRightSesnorHexValue() {
+        return Integer.toHexString(rightSensor);
+    }
+
+    public String getSonarSesnorHexValue() {
+        return Integer.toHexString(sonarSensor);
+    }
+
+
+    ////
+    ////// Class Builder
+    ////
+    public static final class Builder {
+        private int leftSensor;
+        private int frontSensor;
+        private int rightSensor;
+        private int sonarSensor;
+
+        public Builder() {
+        }
+
+        public Builder withLeftSensor(int val) {
+            leftSensor = val;
+            return this;
+        }
+
+        public Builder withFrontSensor(int val) {
+            frontSensor = val;
+            return this;
+        }
+
+        public Builder withRightSensor(int val) {
+            rightSensor = val;
+            return this;
+        }
+
+        public Builder withSonarSensor(int val) {
+            sonarSensor = val;
+            return this;
+        }
+
+        public DataPoint build() {
+            return new DataPoint(this);
+        }
+    }
+}
---
+++ app/src/main/java/edu/cedarville/adld/common/translator/DataPointTranslator.java
@@ -0,0 +1,40 @@
+package edu.cedarville.adld.common.translator;
+
+import edu.cedarville.adld.common.model.DataPoint;
+
+public class DataPointTranslator {
+
+    private static final int LEFT_SENSOR_POS = 0;
+    private static final int FRONT_SENSOR_POS = 1;
+    private static final int RIGHT_SENSOR_POS = 2;
+    private static final int SONAR_SENSOR_POS = 3;
+    private static final int BASE_16 = 16;
+
+    /**
+     * Translates an input in hex format "AA,BB,CC,DD" into a DataPoint model
+     * @param input -   Input from the robot
+     * @return  -   DataPoint model representing the input
+     */
+    public DataPoint translateInputToDataPoint(String input) {
+        String[] values = input.split(",");
+
+        // Currently, all inputs must have 4 values and will always have 4 value
+        if (values.length != 4) {
+            throw new IllegalArgumentException("An input with " + values.length + " values was received. Inputs must have 4 values separated by commas");
+        }
+
+        // Parse each hex value into an integer value
+        int left = Integer.parseInt(values[LEFT_SENSOR_POS], BASE_16);
+        int front = Integer.parseInt(values[FRONT_SENSOR_POS], BASE_16);
+        int right = Integer.parseInt(values[RIGHT_SENSOR_POS], BASE_16);
+        int sonar = Integer.parseInt(values[SONAR_SENSOR_POS], BASE_16);
+
+        // Build and return a DataPoint model with int values
+        return new DataPoint.Builder()
+                .withLeftSensor(left)
+                .withFrontSensor(front)
+                .withRightSensor(right)
+                .withSonarSensor(sonar)
+                .build();
+    }
+}
